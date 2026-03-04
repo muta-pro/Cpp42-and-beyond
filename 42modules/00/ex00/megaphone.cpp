acf62ab1c5b3541f582e075d8af9a9dbe9378379
ivan <i-muta@hotmail.com>
2026-03-04 20:03:37 +0100

    added jurnal

--- 42modules/00/ex00/megaphone.cpp
+++ 42modules/00/ex00/megaphone.cpp
@@ -39,3 +39,40 @@ int main(int argc, char **argv)
 	std::cout << std::endl;
 	return (0);
 }
+
+//modern c++11 variations
+
+#include <iostream>
+#include <cctype>
+#include <string>
+
+int main(int argc, char** argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return 0;
+    }
+
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string s(argv[i]);
+
+        for (char& c : s)
+            c = std::toupper(static_cast<unsigned char>(c));
+
+        std::cout << s;
+    }
+
+    std::cout << '\n';
+}
+
+//or even
+
+for (int i = 1; i < argc; ++i)
+{
+    for (char* p = argv[i]; *p; ++p)
+        std::cout << static_cast<char>(
+            std::toupper(static_cast<unsigned char>(*p))
+        );
+}
