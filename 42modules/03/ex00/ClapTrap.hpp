d6ef0e036b9ed6e463e72f9e9e5bae3eda2b69b3
ivan <i-muta@hotmail.com>
2026-04-01 19:30:24 +0200

    add

--- 42modules/03/ex00/ClapTrap.hpp
+++ 42modules/03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: imutavdz <imutavdz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 13:43:33 by imutavdz          #+#    #+#             */
-/*   Updated: 2026/03/31 14:27:05 by imutavdz         ###   ########.fr       */
+/*   Updated: 2026/04/01 19:03:18 by imutavdz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 class ClapTrap{
 	public:
 			ClapTrap();
-			ClapTrap(const char *_Name);
+			ClapTrap(const std::string &name);
 			ClapTrap(const ClapTrap &copy);
 			ClapTrap &operator=(const ClapTrap &assign);
 			~ClapTrap();
 
-			void attack(const std::string& target);
+			void attack(const std::string &target);
 			void takeDamage(unsigned int amount);
 			void beRepaired(unsigned int amount);
 	private:
 		std::string	_Name;
-		int			HitPoints = 10;
-		int			EnergyPoints = 10;
-		int			AttackDamage = 0;
+		int			_HitPoints;
+		int			_EnergyPoints;
+		int			_AttackDamage;
 };
